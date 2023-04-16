b04277d7d0749650958444cb6c5dbd8dc97505b2
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2023-04-16 17:29:15 -0400

    implement PathVFX Generator (#42)

--- Assets/Scripts/Trigger/TriggerObjectFadeIn.cs
+++ Assets/Scripts/Trigger/TriggerObjectFadeIn.cs
@@ -3,22 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class TriggerObjectFadeIn : TriggerBase
 {
     [Header("指定播放的Animator 如果为空则在当前物体搜animator")]
     [SerializeField] private Animator _animator;
     [SerializeField, ReadOnly] private String triggerName = "FadeInTrigger";
+    [SerializeField] private bool isUsingAnimator = true;
+    
     private List<Transform> _childList;
-
-    // private Renderer _renderer;
+    private Renderer _renderer;
     
     private void Awake()
     {
-        if (_animator == null) _animator = GetComponent<Animator>();
+        if (_animator == null && isUsingAnimator) _animator = GetComponent<Animator>();
 
         _childList = new List<Transform>();
-
+        _renderer = GetComponent<Renderer>();
+        if (_renderer != null) _renderer.enabled = false;
+        
+        
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform childObj = transform.GetChild(i).GetComponent<Transform>();
@@ -40,7 +45,27 @@ public class TriggerObjectFadeIn : TriggerBase
         {
             child.gameObject.SetActive(true);
         }
-
-        _animator.SetTrigger(triggerName);
+        if(_renderer != null) _renderer.enabled = true;
+        if(_animator != null && isUsingAnimator) _animator.SetTrigger(triggerName);
+        
+        
+        
     }
+
+    protected override void ExitEvent()
+    {
+        base.ExitEvent();
+        FadeOut();
+    }
+
+    void FadeOut()
+    {
+        foreach (var child in _childList)
+        {
+            child.gameObject.SetActive(false);
+            if(_renderer != null) _renderer.enabled = true;
+
+        }
+    }
+
 }
