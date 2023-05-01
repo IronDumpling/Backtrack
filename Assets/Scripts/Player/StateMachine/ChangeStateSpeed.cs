2e4fcbd2233f4ebb6ff8bfa5ff484332d7977b25
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-05-01 19:44:14 -0400

    Clean legacy ball animation

---
+++ Assets/Scripts/Player/StateMachine/ChangeStateSpeed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeStateSpeed : StateMachineBehaviour
+{
+    private PlayerMotor _playerMotor;
+
+    // Use this for initialization
+    void Start()
+	{
+        _playerMotor = PlayerController.Instance?.GetComponent<PlayerMotor>();
+	}
+
+    // Update is called once per frame
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        animator.speed = stateInfo.speed * _playerMotor.ZSpeed;
+    }
+}
+
