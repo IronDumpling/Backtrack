a536556bfd81d7cec0ee3b17a2567fc3f948f3b6
Yang Chen <jackchen.chen@mail.utoronto.ca>
2023-05-02 23:32:28 -0400

    Enable Level3 PlayerController

--- Assets/Scripts/Utils/DebugLogger.cs
+++ Assets/Scripts/Utils/DebugLogger.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 
 public static class DebugLogger {
     public static void Error(string objectName, string message) {
+#if UNITY_EDITOR
         Debug.LogError("["+ objectName + "]: " + message);
         Debug.Break();
+#endif
+    }
+
+    public static void Log(string objectName, string message) {
+#if UNITY_EDITOR
+        Debug.Log("[" + objectName + "]: " + message);
+#endif
     }
 }
