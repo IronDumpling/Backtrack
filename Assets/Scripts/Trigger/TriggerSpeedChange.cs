d3cdff311fa75c8ca736a73f2fbb41fbcf0bf88c
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-05-09 12:18:42 -0400

    Add timescale change at choice

--- Assets/Scripts/Trigger/TriggerSpeedChange.cs
+++ Assets/Scripts/Trigger/TriggerSpeedChange.cs
@@ -4,12 +4,16 @@ public class TriggerSpeedChange : TriggerBase
 {
     [SerializeField] private float _ZSpeedChange = 0.5f;
     [SerializeField] private float _XSpeedChange = 0f;
+    [SerializeField] private bool changeBackOnExit = false;
+    [SerializeField] private float _timeScaleInside = 1f;
 
+    private float _timeScaleOutside;
     private PlayerMotor _playerMotor;
 
     private void Start()
     {
         _playerMotor = PlayerController.Instance?.GetComponent<PlayerMotor>();
+        _timeScaleOutside = Time.timeScale;
         if (_playerMotor == null)
             Debug.LogWarning("Player Motor Missing");
     }
@@ -17,11 +21,23 @@ public class TriggerSpeedChange : TriggerBase
     protected override void enterEvent()
     {
         base.enterEvent();
+        _timeScaleOutside = Time.timeScale;
+        Time.timeScale = _timeScaleInside;
         if(_playerMotor != null)
         {
             _playerMotor.ZSpeed += _ZSpeedChange;
             _playerMotor.XSpeed += _XSpeedChange;
         }
-            
+    }
+
+    protected override void ExitEvent()
+    {
+        base.ExitEvent();
+        Time.timeScale = _timeScaleOutside;
+        if (changeBackOnExit && _playerMotor != null)
+        {
+            _playerMotor.ZSpeed -= _ZSpeedChange;
+            _playerMotor.XSpeed -= _XSpeedChange;
+        }
     }
 }
