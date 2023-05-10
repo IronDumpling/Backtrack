d0181b2296980103120907c263553a5d6c15067b
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-05-09 20:51:41 -0400

    Score Could Work

---
+++ Assets/Scripts/UI/LevelInfoUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfoUI : MonoBehaviour
+{
+    private GameObject _collection;
+    private GameObject _title;
+    private GameObject _description;
+
+    [SerializeField] private List<string> _levelTitles;
+    [SerializeField] private List<string> _levelDescriptions;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _title = transform.Find("LevelTitle")?.gameObject;
+        _collection = transform.Find("CollectionUI")?.gameObject;
+        _description = transform.Find("Description")?.gameObject;
+    }
+
+    // Update is called once per frame
+    public void HoverLevel(int levelNum)
+    {
+        _title.GetComponent<TMPro.TMP_Text>().text = $"{_levelTitles[levelNum-1]}";
+        _description.GetComponent<TMPro.TMP_Text>().text = $"{_levelDescriptions[levelNum-1]}";
+        _collection.GetComponent<CollectionUI>().ShowScore(levelNum);
+    }
+}
