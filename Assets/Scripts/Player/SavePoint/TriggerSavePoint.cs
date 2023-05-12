e22c100c1d7b9c2c6668da9d753806f0d5165249
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2023-05-12 01:56:36 -0400

    Feature level3 save (#131)
    * implement move arrow
    
    * failed implement local path move
    
    * implement local arrow path move with dotween pro
    
    * Update TriggerMovableObj.cs
    
    * Update test_P1 LZ 1.unity
    
    * Update TriggerMovableObj.cs
    
    * implement no look at arrow
    
    * implement level3 save point

--- Assets/Scripts/Player/SavePoint/TriggerSavePoint.cs
+++ Assets/Scripts/Player/SavePoint/TriggerSavePoint.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Level3_Track;
 #if UNITY_EDITOR
 using UnityEditor.Build.Player;
 using UnityEditor.SearchService;
@@ -9,7 +10,20 @@ using UnityEngine.SceneManagement;
 
 public class TriggerSavePoint : TriggerBase
 {
+    [SerializeField] private int levelNum = 0;
     protected override void enterEvent(Collider c)
+    {
+        if (levelNum == 3)
+        {
+            saveLevel3();
+        }
+        else
+        {
+            saveLevel0(c);
+        }
+    }
+
+    private void saveLevel0(Collider c)
     {
         string sceneName = SceneManager.GetActiveScene().name;
         Vector3 point = c.transform.position;
@@ -29,5 +43,21 @@ public class TriggerSavePoint : TriggerBase
             scores,
             speed,
             xspeed);
+    }
+
+    private void saveLevel3()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        
+        string bgmName = AudioManager.Instance.GetMusicIsPlaying();
+        float time = AudioManager.Instance.GetMusicTime(bgmName);
+        int scores = ScoreManager.Instance.CurrentScoreInLevel;
+        int trackId = TrackManager.Instance._CurrentTrackIdx;
+        SavePointManager.Instance.SetSavePointLevel3(sceneName,
+            bgmName,
+            time,
+            scores,
+            trackId);
+        
     }
 }
