3ab9841f775f6501f84943f98ee1e7b5878f939c
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2023-05-16 17:44:17 -0400

    fix async load animator (#146)

---
+++ Assets/Scripts/Path/TriggerWindField.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWindField : TriggerBase
+{
+    protected override void StayEvent()
+    {
+        base.StayEvent();
+        //PlayerController_L3.Instance.
+    }
+}
