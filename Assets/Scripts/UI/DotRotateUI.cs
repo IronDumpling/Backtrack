792e046843ca9b323c0c3cda55dc8eb40b07d256
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-05-15 20:55:48 -0400

    Main UI Dot Rotate

--- Assets/Scripts/UI/DotRotateUI.cs
+++ Assets/Scripts/UI/DotRotateUI.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class DotRotateUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float _rotateSpeed = 0.5f;
+
+    private GameObject _circle;
+    private GameObject _dot;
+
+    private RectTransform _rectTrans;
+    private float _radius;
+    private float _angle;
+    bool isBlowUp = false;
+
+    private void Awake()
     {
+        _circle = transform.Find("Circle")?.gameObject;
+        _dot = transform.Find("Dot")?.gameObject;
+        _rectTrans = _dot.GetComponent<RectTransform>();
+        _radius = gameObject.GetComponent<RectTransform>().rect.width / 2;
+    }
+
+    private void Update()
+    {
+        if (isBlowUp) Rotate();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBlowUp()
     {
-        
+        isBlowUp = true;
+        _angle = 0f;
+    }
+
+    public void OnShrinkDown()
+    {
+        isBlowUp = false;
+        _angle = 0f;
+        SetLocalPosition();
+    }
+
+    void Rotate()
+    {
+        SetLocalPosition();
+        _angle += Time.deltaTime * Mathf.PI * _rotateSpeed;
+    }
+
+    void SetLocalPosition()
+    {
+        float x = Mathf.Cos(-_angle) * _radius;
+        float y = Mathf.Sin(-_angle) * _radius;
+        _rectTrans.localPosition = new Vector3(x, y, 0f);
     }
 }
