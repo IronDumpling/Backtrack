f5c8c1a98f6aa6b7803d3ba755a1b440471834d9
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-05-16 10:17:30 -0400

    End UI Updates

--- Assets/Scripts/UI/ChoiceManager.cs
+++ Assets/Scripts/UI/ChoiceManager.cs
@@ -7,11 +7,14 @@ public class ChoiceManager : NoDestroyMonoSingleton<ChoiceManager>
 {
     public string displayText;
     public List<string> choices;
+    private PlayerData_SO _playerData;
 
     protected override void Init()
     {
         base.Init();
         
+        _playerData = Resources.Load<PlayerData_SO>("GameData/PlayerData");
+
         choices = new List<string>();
         for (int i = 0; i < 4; i++)
         {
@@ -26,6 +29,7 @@ public class ChoiceManager : NoDestroyMonoSingleton<ChoiceManager>
         generateString();
         GameObject.Find("Beat_5_Track/End-Track-L/Instruction_Canvas/Instruction").GetComponent<TMPro.TMP_Text>().text = displayText;
         GameObject.Find("Beat_5_Track/End-Track-R/Instruction_Canvas/Instruction").GetComponent<TMPro.TMP_Text>().text = displayText;
+        _playerData.level0Choices = displayText;
     }
 
     public void generateString()
