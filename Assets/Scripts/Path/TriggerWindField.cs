22ffe87c4057476d256460c6d3ad137bee9f4929
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2023-05-17 03:33:22 -0400

    Feature level3 wind field (#147)
    * fix async load animator
    
    * windfield 1

--- Assets/Scripts/Path/TriggerWindField.cs
+++ Assets/Scripts/Path/TriggerWindField.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerWindField : TriggerBase
 {
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = PlayerController_L3.Instance.transform.Find("BirdSlot").GetComponent<Rigidbody>();
+    }
+
     protected override void StayEvent()
     {
         base.StayEvent();
-        //PlayerController_L3.Instance.
+        rb.AddForce(rb.transform.right * 1000f);
     }
 }
