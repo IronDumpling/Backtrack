4d4cd6a17b2ac566d4b7d240523693c99ae6bf68
Xiangfei Kong <61561634+DeusPio@users.noreply.github.com>
2023-05-21 16:16:50 -0400

    design lv4 (#162)

--- Assets/Scripts/Level3_Track/TrackManager.cs
+++ Assets/Scripts/Level3_Track/TrackManager.cs
@@ -56,23 +56,30 @@ namespace Level3_Track {
             curTrack._TrackVirtualCamera.m_Priority = (_CurrentTrackIdx+1);
 
             // TODO: use to lerp to Transform Player Position to TrackStarting point
-            //Vector3 fromPos = _playerController.transform.position;
-            //Vector3 toPos = curTrack._PlayerTrack.m_Waypoints[0].position;
-            //_playerDollyCart.m_Path = null;
-            //DOTween.To(
-            //    () => fromPos,
-            //    x => _playerController.transform.position = fromPos = x,
-            //    toPos,
-            //    1f
-            //).SetEase(DOTween.defaultEaseType)
-            //.onComplete += () => {
-            //    _playerDollyCart.m_Path = curTrack._PlayerTrack;
-            //    _playerDollyCart.m_Position = 0;
-            //};
-            _playerDollyCart.m_Position = 0;
-            _playerDollyCart.m_Path = curTrack._PlayerTrack;
-            
+            Vector3 fromPos = _playerController.transform.position;
+            Vector3 toPos = curTrack.transform.TransformPoint(curTrack._PlayerTrack.m_Waypoints[0].position);
+            Debug.Log("from :" + fromPos);
+            Debug.Log("to :" + toPos);
+            _playerDollyCart.m_Path = null;
+            DOTween.To(
+                () => fromPos,
+                x => _playerController.transform.position = fromPos = x,
+                toPos,
+                1f
+            ).SetEase(DOTween.defaultEaseType)
+            .onComplete += () => {
+                _playerDollyCart.m_Path = curTrack._PlayerTrack;
+                _playerDollyCart.m_Position = 0;
+            };
+            //_playerDollyCart.m_Position = 0;
+            //_playerDollyCart.m_Path = curTrack._PlayerTrack;
+            Quaternion toQT = curTrack._PlayerTrack.EvaluateOrientation(0);
 
+            _playerController.transform.DORotate(toQT.eulerAngles, 1f);
+            
+            
+            //curTrack._PlayerTrack.m_Waypoints[0].
+            
             A_TrackUpdate = TrackNormal;
             StartCoroutine(CamBlendYieldPlyControl(curTrack));
         }
