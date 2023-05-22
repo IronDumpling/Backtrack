1b2d956bedfd2c587a421ef64ef54d867ccfb8c2
Yang Chen <jackchen.chen@mail.utoronto.ca>
2023-05-22 17:02:47 -0400

    add LookAt script

---
+++ Assets/Scripts/Obstacles/LookAt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Obstacles {
+    public class LookAt : MonoBehaviour {
+
+        public Transform LookAtTarget;
+
+        private void Awake() {
+            if (LookAtTarget == null) {
+                DebugLogger.Warning(this.name, "LookAtTarget not set!");
+            }
+        }
+
+        private void Update() {
+            if (LookAtTarget != null)
+                this.transform.LookAt(LookAtTarget);
+        }
+    }
+}
