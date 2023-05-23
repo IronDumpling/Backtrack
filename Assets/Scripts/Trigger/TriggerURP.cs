512044f0add9555c0c34440f7231db9a8dae0946
Chuyue Zhang <70104294+IronDumpling@users.noreply.github.com>
2023-05-22 20:22:08 -0400

    Tried URP Blit But Failed

--- Assets/Scripts/Trigger/TriggerURP.cs
+++ Assets/Scripts/Trigger/TriggerURP.cs
@@ -2,35 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using Cyan;
 
 public class TriggerURP : TriggerBase
 {
-    public UniversalRenderPipelineAsset urpAsset;
-    public string MaterialName;
+    public Blit URPBlit;
+    public string m_MaterialName;
 
     // Start is called before the first frame update
     void Awake()
     {
-        urpAsset = Resources.Load<UniversalRenderPipelineAsset>("Urp/Universal Render Pipeline Asset_Renderer");
+        if (URPBlit == null) Debug.LogError("URP Blit is Missing!");
     }
 
     public void SetBlitMaterial(string materialName)
     {
-        Material material = Resources.Load<Material>("Prefabs/VFX/Vfx/Full Screen" + materialName);
+        if(materialName == "")
+        {
+            URPBlit.settings.blitMaterial = null;
+            return;
+        }
 
-        if (material != null)
-        {
-            //urpAsset.blitMaterial = material;
-        }
-        else
-        {
-            Debug.LogError("Failed to find material: " + materialName);
-        }
+        Material material = Resources.Load<Material>("Prefabs/VFX/Vfx/Full Screen/" + materialName);
+
+        if (URPBlit != null && material != null) URPBlit.settings.blitMaterial = material;
+        else Debug.LogError("Failed to find material: " + materialName);
     }
 
     protected override void enterEvent()
     {
         base.enterEvent();
-        SetBlitMaterial(MaterialName);
+        SetBlitMaterial(m_MaterialName);
     }
 }
