03a6331e11a976f3051cc48039d2a8e38ad7ed6b
小左QAQ <lz1440918141@126.com>
2023-05-30 03:37:05 +0800

    Shake_Change

--- Assets/Scripts/Level3_LZ/FriendClose.cs
+++ Assets/Scripts/Level3_LZ/FriendClose.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class FriendClose : MonoBehaviour
 {
+    private BoxCollider boxCollider;
+    private void Start() {
+        boxCollider = GetComponent<BoxCollider>();
+    }
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "Arrow")
         {
+            boxCollider.enabled = false;
             Destroy(gameObject);
+            Time.timeScale = 1f;
         }
     }
 }
